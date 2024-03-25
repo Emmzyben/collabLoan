1d2c36b5fdb1a431eb3ff7fba34bde090410af2b
unknown <emmco96@gmail.com>
2024-03-25 16:03:48 +0100

    new

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <link rel="stylesheet" href="{{ asset('style.css') }}">
+    </head>
+    <body>
+       <header>
+        <div id="head">
+<div>
+<h3>CollabLoan </h3>
+</div>
+
+<div>
+<ul>
+    <li>Home</li>
+    <li>Offers</li>
+    <li>Create offer</li>
+    <li id="support">Support</li>
+</ul>
+</div>
+</div>
+       </header>
+       <main>
+
+       </main>
+       <footer>
+
+       </footer>
+    </body>
+</html>
